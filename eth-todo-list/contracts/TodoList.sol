b1cccdd6dad27edf6c5caabcc6d6cd6063282487
Harsh Haria <harshharia1234@gmail.com>
2022-06-15 17:03:17 +0530

    Task add function complete. All tests working properly

--- eth-todo-list/contracts/TodoList.sol
+++ eth-todo-list/contracts/TodoList.sol
@@ -16,6 +16,12 @@ contract TodoList{
 
     mapping (uint=> Task) public tasks;
 
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+
     constructor() public{
         createTask("harsh 1");
         // createTask("harsh 2");
@@ -24,6 +30,7 @@ contract TodoList{
     function createTask(string memory _content) public{
         taskCount++;
         tasks[taskCount] = Task(taskCount,_content,false);
+        emit TaskCreated(taskCount,_content,false);
     }
 
 }
--- eth-todo-list/test/TodoList.test.js
+++ eth-todo-list/test/TodoList.test.js
@@ -21,5 +21,16 @@ contract('TodoList', (accounts) => {
         assert.equal(task.completed, false)
         assert.equal(taskCount.toNumber(), 1)
       })
+
+      it('creates tasks', async () => {
+        const result = await this.todoList.createTask('A new task')
+        const taskCount = await this.todoList.taskCount()
+        assert.equal(taskCount, 2)
+        console.log(result)
+        const event = result.logs[0].args
+        assert.equal(event.id.toNumber(), 2)
+        assert.equal(event.content, 'A new task')
+        assert.equal(event.completed, false)
+      })
       
 })
